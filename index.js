8ffca56790ca0f46b9b4a788d827fb5a054c389c
Lê Văn Thông <95867263+RobinThomsonDT@users.noreply.github.com>
2023-05-01 01:12:30 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 5500;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+app.listen(port, (request, response) => {
+    console.log("Server is running...");
+});
