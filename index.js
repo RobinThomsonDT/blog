111b42925648b3715f308f832694460d9d1a2868
Lê Văn Thông <95867263+RobinThomsonDT@users.noreply.github.com>
2023-05-02 16:47:10 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 5500;
 
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  return res.send("<h1>Alo 223</h1>");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
-app.listen(port, (request, response) => {
-    console.log("Server is running...");
+  console.log(`Example app listening on port ${port}`);
 });
