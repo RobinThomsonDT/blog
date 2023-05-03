86e007c2826e1edbce6b0a778b39a82da89c1106
Lê Văn Thông <95867263+RobinThomsonDT@users.noreply.github.com>
2023-05-03 22:11:38 +0700

    ver3

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const path = require("path");
 const morgan = require("morgan");
+const ejs = require("ejs");
+const employee = require("./employee.json");
 const app = express();
-const port = 5500;
-
-app.use(morgan("combined"));
+const port = 6500;
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(morgan("combined")); // http logger
+app.set("view engine", "ejs"); // set view engine
 
 app.get("/", (req, res) => {
-  return res.send("<h1>Alo 223</h1>");
+  res.render("home");
+  // res.status(200).json(employee);
 });
 
 app.listen(port, () => {
